24dea77ecda9a8aded7bc78608199a2fd97ebb59
Justin Ethier <justin.ethier@gmail.com>
2020-03-13 13:13:44 -0400

    Revised

--- package.scm
+++ package.scm
@@ -4,11 +4,11 @@
  (license        "BSD")
  (authors        "Alex Shinn")
  (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
- (description    "QR encoding library")
+ (description    "QP encoding library")
  (tags           "networking")
  (docs           "https://github.com/cyclone-scheme/quoted-printable")
  (test           "test.scm")
 
  (library
      (name (cyclone quoted-printable))
-   (description "QR encoding library")))
+   (description "QP encoding library")))
