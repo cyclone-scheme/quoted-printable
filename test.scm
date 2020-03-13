2a74c3fed9944ea5b61bb616ee86f52566e261a1
Justin Ethier <justin.ethier@gmail.com>
2020-03-13 13:28:23 -0400

    Add truncated text back

--- test.scm
+++ test.scm
@@ -4,8 +4,9 @@
   (test "J'interdis aux marchands de vanter trop leur marchandises. Car ils se font vite pédagogues et t'enseignent comme but ce qui n'est par essence qu'un moyen, et te trompant ainsi sur la route à suivre les voilà bientôt qui te dégradent, car si leur musique est vulgaire ils te fabriquent pour te la vendre une âme vulgaire."
         (quoted-printable-decode-string
          "J'interdis aux marchands de vanter trop leur marchandises. Car ils se font =
-te p=C3=A9dagogues et t'enseignent comme but ce qui n'est par essence qu'=
- moyen, et te trompant ainsi sur la route =C3=A0 suivre les voil=C3=A0 bi=
-t=C3=B4t qui te d=C3=A9gradent, car si leur musique est vulgaire ils te f=
-riquent pour te la vendre une =C3=A2me vulgaire.")))
+vite p=C3=A9dagogues et t'enseignent comme but ce qui n'est par essence qu'=
+un moyen, et te trompant ainsi sur la route =C3=A0 suivre les voil=C3=A0 bi=
+ent=C3=B4t qui te d=C3=A9gradent, car si leur musique est vulgaire ils te f=
+abriquent pour te la vendre une =C3=A2me vulgaire.")))
 (test-exit)
+
