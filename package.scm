45bb44b6469f71ca16c673edf92439b66af79548
Justin Ethier <justin.ethier@gmail.com>
2020-03-13 13:06:47 -0400

    Cleanup

--- package.scm
+++ package.scm
@@ -1,18 +1,14 @@
 (package
- (name           sha2)
+ (name           quoted-printable)
  (version        0.1)
  (license        "BSD")
- (authors        "Alex Shinn, Alexei Lozovsky")
+ (authors        "Alex Shinn")
  (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
- (description    "Native implementation of SHA-2 digest algorithms")
- (tags           "crypto" "hash")
- (docs           "https://github.com/cyclone-scheme/sha2/raw/master/Readme.md")
- (test           "test-sha2.scm")
-
- (program
-   (name sha256sum-cyc)
-   (description "A simple program to compute SHA256 of a file"))
+ (description    "QR encoding library")
+ (tags           "networking")
+ (docs           "https://github.com/cyclone-scheme/quoted-printable")
+ (test           "test.scm")
 
  (library
-     (name (cyclone crypto sha2))
-   (description "SHA-2 library")))
+     (name (cyclone quoted-printable))
+   (description "QR encoding library")))
